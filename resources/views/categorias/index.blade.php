9880ea3b88cf90f9f764f877764f866da1ba1b7f
nankingscolors <andersonmiko@live.com>
2019-09-30 23:16:39 -0500

    Actualizacion Blade 2

--- resources/views/categorias/index.blade.php
+++ resources/views/categorias/index.blade.php
@@ -1,49 +1,20 @@
-<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <metanombre charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Categorias</title>
-    </head>
-    <body>
-        <div class="container">
+@extends('layouts.app')
 
-             
+{{-- con seccion podemos envir diferentes elementos a la pantalla principal, se podria decir que es una manera de decirlo , una aplicacion deuna sola pagina ya que tosdo se ejecuta en el index y comparte todo en las diferentes llamadas --}}
 
-            <div class="row">
-                <h4>No olvidar descomentar para que funcione algunas variables</h4>
-            </div>
-            <div class="row">    --- ENVIO DE INFORMACION SIN U    --- ENVIO DE INFORMACION SIN UTILIZAR SECCION  --}}TILIZAR SECCION  --}}
-                <!-- Se puede enviar datos del cntrolador hacia aqui(la vista) -->
-            <div> Inicio - 1era forma - Enviando datos desde el controlador hacia la vista con interpolacion</div>
-            <p>Hola <strong>{{--$nombre--}}</strong></p>
-            <div> Fin - 1era forma - Enviando datos desde el controlador hacia la vista con interpolacion</div>
-            </div>
-            <br>
-            <div class="row">
-                    <!-- Se puede enviar datos del cntrolador hacia aqui(la vista) -->
-                <div> Inicio - 2da forma - Enviando datos desde el controlador hacia la vista con interpolacion</div>
-                <p>Hola <strong> {{--$variablenombre--}} {{--$variableapellido--}}                </strong></p>
-                <div> Fin - 2da forma - Enviando datos desde el controlador hacia la vista con interpolacion</div>
-                </div>
-                <br>
-                <div class="row">
-                        <!-- Se puede enviar datos del cntrolador hacia aqui(la vista) -->
-                    <div> Inicio - 3ra forma - Enviando datos desde el controlador hacia la vista con interpolacion</div>
-                <p>Hola <strong> {{--$nombredeusuario--}}</strong></p>
-                    <div> Fin - 3ra forma - Enviando datos desde el controlador hacia la vista con interpolacion</div>
-                    </div>
-            <div class="row">
-                <h3>Ciclos parciales y Ciclos Foreach // Lo equivalente que seria en angular a ngFor</h3>
-                {{--Inicio -  Esta es la interpretacion de ngFor para darproductos // Trabaja con ek controlador llamado CategoriasController en la funcion index--}}
-                {{-- para obtener una lista ordenada utiklizamos etiqueta <ol> --}}
-                @include('categorias.partes.lista')
-                {{--Fin -  Esta es la interpretacion de ngFor para darproductos--}}
-            </div>
+@section('titulo')
+    Categorias
+@endsection
 
+@section('contenido')
 
+    <h1>Categorias</h1>
+    <ul>
+        @foreach ($categorias as $categoria)
+            
+        <li>{{ $categoria->nombre}}</li>
 
-        </div>
-    </body>
-    </html>
+        @endforeach
+    </ul>
+    
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('titulo')</title>
+
+    {{-- ingresar a stilos css --}}
+
 </head>
 <body>
-    @include('contenido')
+    @yield('contenido')
 </body>
 </html>
